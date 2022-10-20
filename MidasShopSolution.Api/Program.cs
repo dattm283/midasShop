0a9d948834460e39996837e7c98a646344c08ab2
tmd <dattran2803@gmail.com>
2022-10-20 15:33:00 +0700

    Add swagger

--- MidasShopSolution.Api/Program.cs
+++ MidasShopSolution.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using MidasShopSolution.Application.Catalog.Products;
 using MidasShopSolution.Data.EF;
 
@@ -10,18 +11,31 @@ builder.Services.AddDbContext<MidasShopDbContext>(options => options.UseSqlServe
 ));
 // Declare DI
 builder.Services.AddTransient<IPublicProductService, PublicProductService>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo{Title = "Swagger MidasShop Solution", Version = "v1"});
+});
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger MidasShop Solution V1"));
 }
+
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
